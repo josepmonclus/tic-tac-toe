90662fb6597d571147e2ab3c103e6818bcd828da
Josep Monclus <josep.monclus@gmail.com>
2023-08-11 08:37:25 +0200

    gh-pages setup

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "src",
+    envDir: "../",
     resolve: {
         alias: {
             "@": new URL("./src", import.meta.url).pathname
         }
     },
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        assetsDir: "./",
+        emptyOutDir: true,
     }
 });
